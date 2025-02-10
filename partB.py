80420b9b9f136c889fc742a0267f1b4367ec5c8f
Cianan-C <b00164511@mytudublin.ie>
2025-02-10 18:00:31 +0000

    Completed Par

---
+++ partB.py
@@ -0,0 +1,17 @@
+#Cianán Carey B00164511
+# #10/02/2025
+#A Program to Print Steps 1 to 9 to the Screen
+
+def printSteps(a,b):
+    for i in range(a, b + 1):
+        print(f"Step {i}")
+
+
+def main():
+    a = 1
+    b = 9
+    printSteps(a,b)
+    
+
+
+main()
---
+++ partC.py
@@ -0,0 +1,18 @@
+#Cianán Carey 
+#B00164511
+# #10/02/2025
+#A Program to Print Steps 1 to 9 to the Screen
+
+def printSteps(a,b):
+    for i in range(a, b + 1):
+        print(f"Step {i}")
+
+
+def main():
+    a = int(input("Insert First step: "))
+    b = int(input("Insert Last step: "))
+    printSteps(a,b)
+    
+
+
+main()
